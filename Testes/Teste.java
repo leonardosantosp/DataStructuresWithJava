87957ba6823fa9b616ebc4837032f286a684e548
Leo Santos <leo040604@gmail.com>
2023-11-16 08:28:21 -0300

    feat: adding the insert in array

---
+++ Testes/Teste.java
@@ -0,0 +1,11 @@
+public class Teste {
+  public static void main(String[] args) {
+    Vetor vetor = new Vetor(2);
+    vetor.adciona("elemento 1");
+    vetor.adciona("elemento 2");
+    vetor.adciona("elemento 3");
+
+    vetor.escreve();
+  }
+
+}
